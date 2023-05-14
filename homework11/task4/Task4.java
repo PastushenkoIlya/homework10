a291b11c377f61b43fb40facd47d77c7cd2e3167
illia-pastushenko <nzxt511@gmail.com>
2023-05-14 22:01:43 +0200

    v3

--- homework11/task4/Task4.java
+++ homework11/task4/Task4.java
@@ -1,21 +1,18 @@
 package homework11.task4;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Task4 {
     public static void main(String[] args) {
 
-        Stream<Integer> randomStream = Stream.iterate(4,(seed) -> {
-            int a = 44;
-            int m = 112;
-
-            int i = 2*a*m/8*seed;
-
-            a += 28;
-            m += 8;
-
-            return i*12+1000/244;
+        Stream<Long> randomStream = Stream.iterate(4l,(seed) -> {
+            long a = 25214903917l;
+            long m = 2^48l;
+            long c = 11l;
+            return (a*seed+c)%m;
         });
 
         randomStream.limit(5).peek(System.out::println).collect(Collectors.toSet());
